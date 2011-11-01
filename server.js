a1a6286e3416f87a4c7456940937d7729ea1914e
Sid N <reddy.siddhartha@rocketmail.com>
2011-10-31 21:13:48 -0400

    Expressly setting views dir and view engine.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var _ = require('underscore')._;
 var routes = require('./routes').routes;
 console.log(routes);
 var express = require('express');
+express.set('views', __dirname + '/views');
+express.set('view engine', __dirname + 'haml');
 var server = express.createServer();
 
 _.each(_.keys(routes), function (route) {
