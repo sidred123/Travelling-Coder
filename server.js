18e2f906d84aa2c9bfcfc2c9d27437bfa831d8ab
Sid N <reddy.siddhartha@rocketmail.com>
2011-10-31 21:38:48 -0400

    Trying with basic "Hello World" application.

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
+var http = require('http');
 var _ = require('underscore')._;
+var express = require('express');
+
 var routes = require('./routes').routes;
 console.log(routes);
-var express = require('express');
-express.set('views', __dirname + '/views');
-express.set('view engine', __dirname + 'haml');
+
 var server = express.createServer();
 
 _.each(_.keys(routes), function (route) {
 	server.get(route, routes[route]);
 });
-
-server.use(express.staticProvider(__dirname + '/public'));
 
 server.listen(12371);
