e01b94b4d27a3ce8f4bd10d27ef92e13a3489015
Sid N <reddy.siddhartha@rocketmail.com>
2011-11-14 08:13:45 -0500

    Error checking when opening the db.

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 var mongo = require('mongodb');
 
 var db = new mongo.Db('travelling_coder', new mongo.Server('localhost', 27017, {}, {}));
-db.open(function () {
+db.open(function (err) {
+    if (err !== null) {
+        console.log(err);
+    }
     console.log("mongo db is now open.");
 });
 
