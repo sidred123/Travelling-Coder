339ad840ecc27c7025a3b1f06c81ab34c9e45a5f
Sid N <reddy.siddhartha@rocketmail.com>
2012-03-17 18:21:48 -0400

    Connecting to mongolab.

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongo = require('mongodb');
 
-var db = new mongo.Db('travelling_coder', new mongo.Server('localhost', 27017, {}, {}));
+var db = new mongo.Db('travelling_coder', new mongo.Server('ds031587.mongolab.com', 31587, {}, {}));
 db.open(function (err) {
     if (err !== null) {
         console.log(err);
